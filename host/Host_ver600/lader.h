b79bd773ace13067b557ddc56ab960008f813874
hdh3296 <hdh3296@gmail.com>
2017-05-04 14:49:46 +0900

    v6.16 폴더 삭제

--- host/Host_ver600/lader.h
+++
@@ -1,8 +0,0 @@
-
-
-extern  void    __attribute__((section(".usercode"))) LdDataTx(void);
-extern  void    __attribute__((section(".usercode"))) LdCmdSet(void);
-extern  void  __attribute__((section(".usercode"))) NewLoaderChk(void);
-
-
-extern unsigned int Ladder_mode;
